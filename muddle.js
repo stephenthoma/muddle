57f558f8976966cd79da11745049b1931630b1cb
sthoma <sthoma4@gmail.com>
2017-06-11 14:55:20 -0600

    Switch to 4 spaces.

--- muddle.js
+++ muddle.js
@@ -1,35 +1,35 @@
 function add_trigger(trigger_id, modal_id) {
-  document.getElementById(trigger_id).addEventListener('click', function() {
-    muddle.open(modal_id);
-  });
+    document.getElementById(trigger_id).addEventListener('click', function() {
+        muddle.open(modal_id);
+    });
 }
 
 var muddle = (function() {
-  var self = {};
+    var self = {};
 
-  window.addEventListener('load', function() {
-    ['muddle-overlay', 'md-close'].map(function(elementClass) {
-      document.getElementsByClassName(elementClass)[0].onclick = function() {
-        self.close();
-      };
+    window.addEventListener('load', function() {
+        ['muddle-overlay', 'md-close'].map(function(elementClass) {
+            document.getElementsByClassName(elementClass)[0].onclick = function() {
+                self.close();
+            };
+        });
     });
-  });
 
-  self.open = function(modal_id) {
-    muddle.close();
+    self.open = function(modal_id) {
+        muddle.close();
 
-    var modal = document.getElementById(modal_id);
-    modal.classList.add('md-active');
-    document.body.classList.add('md-active');
-  };
+        var modal = document.getElementById(modal_id);
+        modal.classList.add('md-active');
+        document.body.classList.add('md-active');
+    };
 
-  self.close = function() {
-    var modal = document.getElementsByClassName('md-active')[1];
-    if (modal !== undefined) {
-      modal.classList.remove('md-active');
-      document.body.classList.remove('md-active');
-    }
-  };
+    self.close = function() {
+        var modal = document.getElementsByClassName('md-active')[1];
+        if (modal !== undefined) {
+            modal.classList.remove('md-active');
+            document.body.classList.remove('md-active');
+        }
+    };
 
-  return self;
+    return self;
 }());
